492ed6ad8e4549f1b4606085d756f8eb1ad2d029
HarromPS <anirudhsharma0192837465@gmail.com>
2023-10-06 21:51:06 +0530

    Updated backend

--- models/certificate.js
+++ models/certificate.js
@@ -2,11 +2,24 @@ const mongoose = require("mongoose");
 
 const CertificateSchema = mongoose.Schema(
   {
-    uid: { type: String, required: true },
-    ucode: { type: Number, required: true },
-    recipent: { type: String, required: true },
-    issued: { type: Date },
-    created: { type: Date, default: Date.now },
+    uid: {
+      type: String,
+      required: true
+    },
+    ucode: {
+      type: Number,
+      required: true
+    },
+    recipent: {
+      type: String,
+      required: true
+    },
+    issued: {
+      type: Date
+    },
+    created: {
+      type: Date, default: Date.now
+    },
   },
   { collection: "certificates" }
 );
